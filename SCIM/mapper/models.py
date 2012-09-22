e86b6dd7a8d0220a23c4ba596abde8d3352d23b5
Smári McCarthy <smari@immi.is>
2012-09-22 11:48:26 +0000

    Model for SCIM added. Updated .gitignore to ignore .sqlite files. Real settings.

--- SCIM/mapper/models.py
+++ SCIM/mapper/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Map(models.Model):
+	name		= models.CharField(max_length=100)
+	user		= models.ForeignKey(User)
+
+
+class EntityClass(models.Model):
+	"""Individual, Group, Organization, State"""
+	name		= models.CharField(max_length=100)
+
+
+class Need(models.Model):
+	"""Six Ways to Die, plus other stuff."""
+	name		= models.CharField(max_length=100)
+	entityclass	= models.ForeignKey(EntityClass)
+
+
+class Tier(models.Model):
+	"""Individual, Household, Region, Country, World, etc."""
+	name		= models.CharField(max_length=100)
+
+
+class ServiceProvider(models.Model):
+	"""Electricity company, supermarket, etc."""
+	map		= models.ForeignKey(Map)
+	name		= models.CharField(max_length=100)
+	tier		= models.ForeignKey(Tier)
+
+
+class Resource(models.Model):
+	"""Food, electricity, etc."""
+	map		= models.ForeignKey(Map)
+	name		= models.CharField(max_length=100)
+	needs		= models.ManyToManyField(Need)
+	serviceprovider = models.ManyToManyField(ServiceProvider)
+	dependency	= models.ManyToManyField('Resource')
+
--- SCIM/settings.py
+++ SCIM/settings.py
@@ -1,5 +1,9 @@
 # Django settings for SCIM project.
 
+import os
+here = lambda x: os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), x))
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,8 +15,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': here('scim.sqlite'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,7 +31,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Atlantic/Reykjavik'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -45,18 +49,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = here('media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = here('static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -115,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mapper',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
