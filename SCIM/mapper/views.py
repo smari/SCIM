554073ae01b84843499a5ceae4d07bd2785fcff5
Elisheva Sterling <lishevita@alwayssababa.com>
2012-09-23 14:56:51 +0100

    committing the piece that  makes the dict come in correctly

--- SCIM/mapper/views.py
+++ SCIM/mapper/views.py
@@ -55,10 +55,9 @@ def get_resources(request, tier, need):
 	ctx = {}
 	t = Tier.objects.get(id=tier)
 	n = Need.objects.get(id=need)
-	ctx["resources"] = list(Resource.objects.filter(serviceprovider__tier = t, needs = n))
+	ctx["resources"] = [x.todict() for x in Resource.objects.filter(serviceprovider__tier = t, needs = n)]
 		
 	return HttpResponse(json.dumps(ctx))
-
 
 
 @login_required
